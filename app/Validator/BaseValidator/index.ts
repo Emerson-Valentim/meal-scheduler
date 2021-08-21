be8f0f1c9bee29b7949de87ecddb81c56fee388b
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-21 19:19:04 -0300

    feat: add base validator and exception handler

---
+++ app/Validator/BaseValidator/index.ts
@@ -0,0 +1,24 @@
+import HttpException from 'App/Exceptions/HttpException'
+
+export default abstract class BaseValidator {
+
+  public static async validate(body, validator, validatorMethod: string ) {
+    try {
+      const schema = await validator[validatorMethod]().validateAsync(body)
+      return schema
+    } catch (error) {      
+      const detailedError = this.buildError(error)
+      throw new HttpException(detailedError.message.trim(), detailedError.statusCode)
+    }
+  }
+
+  private static buildError(error) {
+    return error.details.reduce((errorMessage, detail) => {
+      errorMessage.message += `${detail.message} `
+      return errorMessage
+    }, {
+      statusCode: 400,
+      message: ''
+    })
+  }
+}
--- router.ts
+++ router.ts
@@ -1,8 +1,9 @@
 import EstablishmentController from 'App/Controllers/EstablishmentController';
-import HttpController from 'App/Controllers/HttpController';
+import CrudController, { BaseHttpResponse } from 'App/Controllers/Base/CrudController';
 import BodyMiddleware from 'App/Middleware/BodyMiddleware';
 
 import Utils from 'App/Services/Utils';
+import ExceptionMiddleware from 'App/Middleware/ExceptionMiddleware';
 
 type MethodDefinition = {
   [prefix:string]: string
@@ -11,7 +12,7 @@ type MethodDefinition = {
 type RouteDefinition = {
   [prefix: string]: {
     methods: MethodDefinition
-    controller: HttpController
+    controller: CrudController<any, any>
   }
 }
 
@@ -26,10 +27,29 @@ const routes: RouteDefinition = {
 
 Object.entries(routes).forEach(([prefix, methodDefinition]) => {
   Object.values(methodDefinition.methods).forEach((controllerMethod) => {
-    module.exports[`${prefix}${Utils.capitalize(controllerMethod)}`] = async (event, context) => {
+    module.exports[`${prefix}${Utils.capitalize(controllerMethod)}`] = async (event, context): BaseHttpResponse => {
+      
+      let response: BaseHttpResponse = {
+        statusCode: 500,
+        body: {
+          message: 'Internal server error'
+        }
+      }
+
       BodyMiddleware.requestParser(event, context)
-      const response = await methodDefinition.controller[controllerMethod as any](event, context)
+      
+      try {
+
+        response = await methodDefinition.controller[controllerMethod as any](event, context)
+      
+      } catch( error) {
+      
+        ExceptionMiddleware.handle(response, error);
+      
+      }
+      
       BodyMiddleware.responseParser(response, context)
+      
       return response
     }
   })
