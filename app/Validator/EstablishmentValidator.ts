7ac7981a307fcd837a2b6ba920b0c0fbbccc1efe
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-21 19:19:21 -0300

    feat: add model and validator

---
+++ app/Validator/EstablishmentValidator.ts
@@ -0,0 +1,25 @@
+
+import { BaseCrudValidator } from 'App/Controllers/Base/CrudController'
+import Joi from 'joi'
+
+export default class EstablishmentValidator implements BaseCrudValidator {
+
+  public createValidation() {
+    return Joi.object({
+      name: Joi.string()
+        .required()
+    })
+  }
+
+  public filterValidation() {
+    throw new Error('Method not implemented.')
+  }
+  
+  public updateByIdValidation() {
+    throw new Error('Method not implemented.')
+  }
+
+  public deleteByIdValidation() {
+    throw new Error('Method not implemented.')
+  }
+}
