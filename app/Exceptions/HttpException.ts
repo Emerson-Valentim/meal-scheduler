7339701930d8e020e84ef79003f044354d28e3b9
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-22 16:22:47 -0300

    feat: add level on loggers

--- app/Exceptions/HttpException.ts
+++ app/Exceptions/HttpException.ts
@@ -3,7 +3,11 @@ export default class HttpException extends Error {
   public statusCode: number
   public message: string
 
-  constructor(message: string, status: number) {
+  constructor(message: string, status: number, error) {
+    console.log(JSON.stringify({
+      level: 'ERROR',
+      message: error.message
+    }))
     super(message)
     this.statusCode = status
     this.message = message
