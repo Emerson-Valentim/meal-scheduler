29f05067dd0a7bb6a48b66450913451707bfa76f
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-28 13:22:05 -0300

    feat: add create user validation

--- app/Validator/UserValidator.ts
+++ app/Validator/UserValidator.ts
@@ -4,7 +4,29 @@ import Joi from 'joi'
 
 export default class UserValidator implements BaseCrudValidator {
   createValidation() {
-    throw new Error('Method not implemented.')
+    return Joi.object({
+      cnpj: Joi
+        .string()
+        .required()
+        .min(14)
+        .regex(/^[0-9]*$/)
+        .message('CNPJ is invalid'),
+      phone: Joi
+        .string()
+        .required()
+        .min(13)
+        .max(14)
+        .regex(/^[0-9]*$/)
+        .message('Phone is invalid'),
+      password: Joi
+        .string()
+        .required()
+        .min(8)
+        .max(16),
+      establishment_id: Joi
+        .number()
+        .optional()
+    }).required()
   }
   filterValidation() {
     throw new Error('Method not implemented.')
