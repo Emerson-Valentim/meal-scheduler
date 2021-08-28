119f3c947f54ebde17c9045ef938c6d3b97c4bbd
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-28 16:32:16 -0300

    feat: update schedule validator and add test cases

--- app/Validator/ScheduleValidator.ts
+++ app/Validator/ScheduleValidator.ts
@@ -1,12 +1,36 @@
 
 import { BaseCrudValidator } from 'App/Controllers/Base/CrudController'
+import { WeekDays } from 'App/Models/Schedule'
 import Joi from 'joi'
 
+const hourInterval = {
+  start: Joi
+    .string()
+    .min(8)
+    .max(8)
+    .optional()
+    .regex(/^([01]?\d|2[0-3]):([0-5]?\d):([0-5]?\d)$/),
+  end:
+    Joi
+      .string()
+      .min(8)
+      .max(8)
+      .optional()
+      .regex(/^([01]?\d|2[0-3]):([0-5]?\d):([0-5]?\d)$/)
+}
 export default class ScheduleValidator implements BaseCrudValidator {
   public createValidation() {
     return Joi.object({
       definition: Joi
-        .any()
+        .object({
+          [WeekDays.MONDAY]: hourInterval,
+          [WeekDays.TUESDAY]: hourInterval,
+          [WeekDays.WEDNESDAY]: hourInterval,
+          [WeekDays.THURSDAY]: hourInterval,
+          [WeekDays.FRIDAY]: hourInterval,
+          [WeekDays.SATURDAY]: hourInterval,
+          [WeekDays.SUNDAY]: hourInterval,
+        })
     })
   }
 
