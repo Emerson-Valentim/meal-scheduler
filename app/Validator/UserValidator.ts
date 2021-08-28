88c4364e0981b2f2ff829f7b121f9397c0878d09
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-28 11:18:24 -0300

    feat: add user model

---
+++ app/Validator/UserValidator.ts
@@ -0,0 +1,19 @@
+
+import { BaseCrudValidator } from 'App/Controllers/Base/CrudController'
+import Joi from 'joi'
+
+export default class UserValidator implements BaseCrudValidator {
+  createValidation() {
+    throw new Error('Method not implemented.')
+  }
+  filterValidation() {
+    throw new Error('Method not implemented.')
+  }
+  updateByIdValidation() {
+    throw new Error('Method not implemented.')
+  }
+  deleteByIdValidation() {
+    throw new Error('Method not implemented.')
+  }
+
+}
