20559e229298f8b567a88757a00c6bc58a80def3
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-28 11:18:12 -0300

    feat: add schedule model

---
+++ app/Repository/ScheduleRepository.ts
@@ -0,0 +1,8 @@
+import Schedule from 'App/Models/Schedule';
+import {EntityRepository, Repository} from 'typeorm';
+
+
+@EntityRepository(Schedule)
+export default class ScheduleRepository extends Repository<Schedule> {
+
+}
