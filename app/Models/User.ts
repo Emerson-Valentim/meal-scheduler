6e74c9a3868b7147a8e30a064a606aad9d010b8e
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-08-28 17:01:58 -0300

    feat: add unique key on cnpj

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,7 +1,8 @@
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
 import Establishment from './Establishment';
 
 @Entity()
+@Unique(['cnpj'])
 export default class User {
 
   @PrimaryGeneratedColumn()
