752486771301d2dc6b076470b74e6a6fef2496b9
Emerson Valentim <emerson.santos@smarkio.com.br>
2021-09-07 13:05:42 -0300

    feat: add reservation interval service on create method

--- app/Controllers/ReservationController.ts
+++ app/Controllers/ReservationController.ts
@@ -1,9 +1,15 @@
 import Reservation from 'App/Models/Reservation'
+import ReservationRepository from 'App/Repository/ReservationRepository'
+import CheckSchedule from 'App/Services/CheckSchedule'
+import Utils from 'App/Services/Utils'
+import BaseValidator from 'App/Validator/BaseValidator'
 import ReservationValidator from 'App/Validator/ReservationValidator'
-import CrudController from './Base/CrudController'
+import { APIGatewayEvent } from 'aws-lambda'
+import CrudController, { BaseHttpResponse } from './Base/CrudController'
 
 export default class ReservationController extends CrudController<
   ReservationValidator,
+  ReservationRepository,
   Reservation
 >{
 
@@ -14,4 +20,21 @@ export default class ReservationController extends CrudController<
     )
   }
 
+  public async create({ body }: APIGatewayEvent): Promise<BaseHttpResponse> {
+    try {
+      const data = await BaseValidator.validate(body, this.validator, 'createValidation')
+
+      const model = await this.repository.create(data)
+
+      await this.repository.populate(model, ['establishment.schedule'])
+
+      await CheckSchedule.isIntervalAvailable(model, this.repository)
+
+      await this.repository.persistAndFlush(model)
+
+      return Utils.toHttpResponse(201, model)
+    } catch (error) {
+      throw error
+    }
+  }
 }
